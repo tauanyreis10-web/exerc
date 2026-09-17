3fc4be2a6256ebf0d3a1a318b5be82ff5c95d31b
tauanyreis10-web <tauanyreis10@gmail.com>
2026-09-17 13:57:10 -0300

    tauany3

--- js/script.js
+++ js/script.js
@@ -15,9 +15,29 @@ const jump=() => {
 
 const loop = setInterval(() => {
 
+    console.log ('loop')
+
 const pipePosition = pipe.offsetLeft;
+const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+
+console.log(marioPosition)
+
 console.log(pipePosition)
 
+if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    pipe.style.animation = 'none';
+    pipe.style.bottom = `${marioPosition}px`;
+
+    mario.src = './images/game-over.png';
+    mario.style.width = '75px'
+    mario.style.marginLeft= '50px'
+
+    clearInterval(loop);
+}
 }, 10);
 
 
